a788e7355af880080cb6c1777c961b0554adafd7
Trevor Chartier <chartier.trevor@gmail.com>
2024-07-24 14:03:18 -0600

    Created paw_planner_db and tables schemas

---
+++ db-scripts/create-tables.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS paw_planner_db;
+CREATE DATABASE paw_planner_db;
+
+USE paw_planner_db;
+
+CREATE TABLE pets (
+	pet_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE tasks (
+	task_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    due_date DATE NOT NULL,
+    pet_id INT NOT NULL,
+    FOREIGN KEY (pet_id) REFERENCES pets(pet_id),
+    INDEX idx_pet_id (pet_id)
+    )
+    
