11174487fd672a60a3c478ee449d0cc76521de39
Trevor Chartier <chartier.trevor@gmail.com>
2024-07-24 10:42:07 -0600

    changed from react to vanilla JavaScript, HTML, CSS for frontend

---
+++ frontend/src/js/includeHTML.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", includeAllHTML);
+console.log("HI")
+async function includeAllHTML() {
+  try {
+    await includeHTML("header-placeholder", "../src/components/header.html");
+    await includeHTML("sidebar-placeholder", "../src/components/sidebar.html");
+    await includeHTML("feed-placeholder", "../src/components/feed.html");
+  } catch (error) {
+    console.error("Error including HTML files:", error);
+  }
+}
+
+async function includeHTML(placeholderId, filePath) {
+  try {
+        const response = await fetch(filePath);
+        const data = await response.text();
+        return (document.getElementById(placeholderId).innerHTML = data);
+    } catch (error) {
+        return console.error("Error loading HTML:", error);
+    }
+}
