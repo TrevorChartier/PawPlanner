61f52bcd8237d926038d5cbff67f6209b91373f2
Trevor Chartier <chartier.trevor@gmail.com>
2024-07-30 15:01:26 -0600

    Connected frontend to backend API

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import petRoutes from './routes/pet-routes.js';
 import taskRoutes from './routes/task-routes.js';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500'  // Middleware to allow requests from frontend URL
+}));
 
 app.use('/api/pets', petRoutes);
 app.use('/api/tasks', taskRoutes);
